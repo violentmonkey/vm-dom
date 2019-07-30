94d13a33cbe9c95a7f5ea617343a2aadcf95c2c3
Gerald <gera2ld@163.com>
2019-07-30 10:47:46 +0800

    fix: create HTML element explicitly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export { createElement, Fragment } from '@gera2ld/jsx-dom';
+import { createElement, Fragment } from '@gera2ld/jsx-dom';
+
+const NS_HTML = 'http://www.w3.org/1999/xhtml';
+
+createElement.createElement = tag => document.createElementNS(NS_HTML, tag);
+
+export { createElement, Fragment };
