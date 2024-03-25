c9f9e9525d14be22e063b3f3b3a608742c0114af
Gerald <code@gera2ld.space>
2024-03-25 12:23:32 +0800

    chore: fix versions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,11 +12,16 @@ const outputOptions = {
   indent: false,
   banner: `/*! ${pkg.name}@${pkg.version} | ${pkg.license} License */`,
 };
+const replaceValues = {
+  'process.env.VERSION': pkg.version,
+};
 
 export default defineConfig([
   {
     input: 'src/index.ts',
-    plugins: definePlugins({}),
+    plugins: definePlugins({
+      replaceValues,
+    }),
     external,
     output: {
       format: 'esm',
@@ -26,7 +31,9 @@ export default defineConfig([
   },
   {
     input: 'src/index.ts',
-    plugins: definePlugins({}),
+    plugins: definePlugins({
+      replaceValues,
+    }),
     output: {
       format: 'iife',
       file: `dist/index.js`,
@@ -37,7 +44,9 @@ export default defineConfig([
   },
   {
     input: 'src/solid.ts',
-    plugins: definePlugins({}),
+    plugins: definePlugins({
+      replaceValues,
+    }),
     output: {
       format: 'iife',
       file: `dist/solid.js`,
